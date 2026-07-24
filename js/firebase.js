678d7f3ab1eb21d2ad0ef718fb5259df4f8d76ea
Mariela Herrera <mariela08.mh@gmail.com>
2026-07-23 19:39:40 -0600

    Conecto Firebase y preparo estructura de datos

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCmgkJLAj71qvQwgjIbKHAkSTGI_hKlWfQ",
+  authDomain: "el-jardin-de-renata.firebaseapp.com",
+  projectId: "el-jardin-de-renata",
+  storageBucket: "el-jardin-de-renata.firebasestorage.app",
+  messagingSenderId: "1029198059528",
+  appId: "1:1029198059528:web:97c520ffd156b6144719bd"
+};
+const app = initializeApp(firebaseConfig);
